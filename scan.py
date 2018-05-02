d9f5fb270b73602d9e18f596ca6b1a55ad48807f
Molly <molly.higgins@medium.one>
2018-05-02 14:34:23 -0700

    modified scanner to only print useful information to the user

--- scan.py
+++ scan.py
@@ -8,7 +8,6 @@ def scan():
         scanner = Scanner(0)
         devices = scanner.scan(3)  # List of ScanEntry objects
         for dev in devices:
-            print(dev)
             for (adtype, desc, value) in dev.getScanData():
                 if "Thunder Sense" in desc:
                     print("addr {}, addrtype {}, value {}".format(dev.addr, dev.addrType, value))
